342a03f196798a386925e718bf5d1bfc53ece429
Rodolfo Parada <rodolfo.parada.gonzalez>
2025-04-14 15:31:37 -0400

    hamburguesa 2

--- js/nav.js
+++ js/nav.js
@@ -8,38 +8,38 @@ class Navegacion extends HTMLElement {
     this.shadowRoot.innerHTML = `
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
       <link rel="stylesheet" href="css/nav.css" />
-      <style>
-        .collapse { display: none; }
-        .collapse.show { display: block; }
-      </style>
-      <nav class="navbar navbar-expand-lg navbar-light fixed-top color">
+    
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top color px-3">
         <div class="container-fluid">
-          <a class="titulo-rodolfo fw-bold fs-5" href="#">Rodolfo Parada González</a>
+          <a class="titulo-rodolfo fw-bold fs-5 text-white" href="#">Rodolfo Parada González</a>
           <button class="navbar-toggler" id="btn-toggle" type="button">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="menu">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item"><a class="nav-link sobreMi" href="index.html">Sobre mí</a></li>
-              <li class="nav-item"><a class="nav-link experiencia" href="experiencia.html">Experiencia</a></li>
-              <li class="nav-item"><a class="nav-link formacion" href="formacion.html">Formación</a></li>
-              <li class="nav-item"><a class="nav-link proyectos" href="proyectos.html">Proyectos</a></li>
+              <li class="nav-item"><a class="nav-link text-white" href="index.html">Sobre mí</a></li>
+              <li class="nav-item"><a class="nav-link text-white" href="experiencia.html">Experiencia</a></li>
+              <li class="nav-item"><a class="nav-link text-white" href="formacion.html">Formación</a></li>
+              <li class="nav-item"><a class="nav-link text-white" href="proyectos.html">Proyectos</a></li>
             </ul>
             <img src="assets/images/rodolfo1.jpg" alt="Perfil" class="rounded-circle img-perfil"
               style="width: 40px; height: 40px; object-fit: cover; margin-left: auto;">
           </div>
         </div>
       </nav>
+    
     `;
 
-    // Comportamiento del toggle personalizado
-    const toggleBtn = this.shadowRoot.getElementById("btn-toggle");
-    const menu = this.shadowRoot.getElementById("menu");
-
-    toggleBtn.addEventListener("click", () => {
-      menu.classList.toggle("show");
-    });
-  }
+     // Comportamiento del toggle personalizado
+     const toggleBtn = this.shadowRoot.getElementById("btn-toggle");
+     const menu = this.shadowRoot.getElementById("menu");
+ 
+     toggleBtn.addEventListener("click", () => {
+       menu.classList.toggle("show");
+     });
+ 
+     console.log("Componente <mi-navegacion> cargado correctamente");
+   }
 }
 
 customElements.define("mi-navegacion", Navegacion);
